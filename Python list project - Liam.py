9afd8a7bf827f55cfb39daf40bc28d80b1c0bad2
2BlueFedoras <blucat.wm@gmail.com>
2026-01-08 20:41:04 -0600

    Add files via upload

---
+++ Python list project - Liam.py
@@ -0,0 +1,8 @@
+print('''exampleList = ['armor', 'weapon', 'potion']
+backupList = exampleList.copy()
+exampleList = exampleList.clear()
+print(backupList)''')
+exampleList = ['armor', 'weapon', 'potion']
+backupList = exampleList.copy()
+exampleList = exampleList.clear()
+print(backupList)
